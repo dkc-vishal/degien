589b8bad1b6974c68f06268143928a050d81b3a9
dkc-vishal <vishal.sharma@dkcexport.co.in>
2025-06-21 12:34:19 +0530

    visal

--- app/Tech spec/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import Table from "@/components/Table";
-
-export default function TechSpecSheet() {
-  return <Table />;
-}
--- app/tech-spec/page.tsx
+++ app/tech-spec/page.tsx
@@ -26,15 +26,12 @@ export default function TechSpecSheet() {
     <>
       <div className="flex h-screen font-sans bg-gray-100">
         {/* Sidebar */}
-        <Sidebar />
 
         {/* Main Content */}
         <div
           className="flex-1 flex flex-col"
-          style={{ width: "calc(100vw - 15%)" }}
+          
         >
-          {/* Navbar */}
-          <Header />
 
           {/* Form inputs */}
           <div className="p-6">
