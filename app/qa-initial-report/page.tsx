8fe7d3802fc65d9a0b5c084388252b954b96bf78
prince-dkc <princekumar.singh@dkcexport.co.in>
2025-06-21 12:41:34 +0530

    updates in qa-inital-report route

--- app/qa-initial-report/page.tsx
+++ app/qa-initial-report/page.tsx
@@ -1,34 +1,18 @@
 "use client";
 
-import Header from "@/components/core/Header";
 import SheetTitle from "@/components/core/SheetTitle";
-import Sidebar from "@/components/core/Sidebar";
 import Table from "@/components/core/Table";
 
 export default function QaIntialReport() {
   return (
     <>
-      <div className="flex h-screen font-sans bg-gray-100">
-        {/* Sidebar */}
-        <Sidebar />
-
-        {/* Main Content */}
-        <div
-          className="flex-1 flex flex-col"
-          style={{ width: "calc(100vw - 15%)" }}
-        >
-          {/* Navbar */}
-          <Header />
-
-          {/* Form inputs */}
-          <div className="p-6">
-            <SheetTitle title="QA Intial Report" version="v1.4" />
-          </div>
-
-          {/* Page Content */}
-          <Table />
-        </div>
+      {/* Form inputs */}
+      <div className="p-6">
+        <SheetTitle title="QA Intial Report" version="v1.4" />
       </div>
+
+      {/* Page Content */}
+      <Table />
     </>
   );
 }
