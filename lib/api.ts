2cc93ab0221d979dfc164842cbbe81346bb7ac07
ThokchomNanaocha <thokchomnanaocha@dkcexport.co.in>
2025-07-11 09:52:05 +0530

    api

--- lib/api.ts
+++ lib/api.ts
@@ -1,48 +1,50 @@
-export const BASE_URL = "http://shivam-mac.local:8001/api/v1.0/auth";
+// export const BASE_URL = "http://gulab.local:8000/api/v1.0/auth";
+
+export const BASE_URL = "http://gulab.local:8000/api/";
 
 export const API_ENDPOINTS = {
   login: {
-    url: `${BASE_URL}/login/`,
+    url: `${BASE_URL}v1.0/auth/login/`,
     method: "POST",
   },
   departments: {
-    url: `${BASE_URL}/departments/`,
+    url: `${BASE_URL}v1.0/auth/departments/`,
     method: "GET",
   },
   createUser: {
-    url: `${BASE_URL}/create-user/`,
+    url: `${BASE_URL}v1.0/auth/create-user/`,
     method: "POST",
   },
   resetPasswordAdmin: {
-    url: `${BASE_URL}/reset-password/admin/`,
+    url: `${BASE_URL}v1.0/auth/reset-password/admin/`,
     method: "POST",
   },
   resetPasswordSelfRequest: {
-    url: `${BASE_URL}/reset-password/self/request/`,
+    url: `${BASE_URL}v1.0/auth/reset-password/self/request/`,
     method: "POST",
   },
   resetPasswordSelfChange: {
-    url: `${BASE_URL}/reset-password/self/change`,
+    url: `${BASE_URL}v1.0/auth/reset-password/self/change`,
     method: "POST",
   },
   userProfile: {
-    url: `${BASE_URL}/user-detail/`,
+    url: `${BASE_URL}v1.0/auth/user-detail/`,
     method: "GET",
   },
   updateUsername: {
-    url: `${BASE_URL}/user-detail/`,
+    url: `${BASE_URL}v1.0/auth/user-detail/`,
     method: "POST",
   },
   updateUserDetailsAdmin: (id: string) => ({
-    url: `${BASE_URL}/update-details/admin/${id}/`,
+    url: `${BASE_URL}v1.0/auth/update-details/admin/${id}/`,
     method: "POST",
   }),
   changePasswordSelf: {
-    url: `${BASE_URL}/change-password/`,
+    url: `${BASE_URL}v1.0/auth/change-password/`,
     method: "POST",
   },
   changeSystemPassword: {
-    url: `${BASE_URL}/change-system-password/`,
+    url: `${BASE_URL}v1.0/auth/change-system-password/`,
     method: "POST",
   },
 };
